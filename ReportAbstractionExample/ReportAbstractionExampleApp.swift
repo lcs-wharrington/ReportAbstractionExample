f9c7b896d6becd38e9b2a2a6e26d740c8f127664
Russell Gordon <rgordon@lcs.on.ca>
2021-11-23 13:03:38 -0500

    Initial Commit

---
+++ ReportAbstractionExample/ReportAbstractionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportAbstractionExampleApp.swift
+//  ReportAbstractionExample
+//
+//  Created by Russell Gordon on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct ReportAbstractionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
